4aae99fb05b84e95c706a5a67596ac507111fb2b
raed <eng.salhab@gmail.com>
2021-09-20 08:36:26 +0300

    Solution for exercise R-2.9

---
+++ Data_structures_and_Algorithms_in_python/ch2/R_2_9.py
@@ -0,0 +1,58 @@
+"""R-2.9 Implement the sub method for the Vector class of Section 2.3.3, so
+that the expression u−v returns a new vector instance representing the
+difference between two vectors."""
+
+
+class Vector:
+    """Represent a vector in a multidimensional space."""
+
+    def __init__(self ,d):
+        """Create d-dimensional vector of zeros."""
+        self._coords = [0] * d
+
+    def __len__(self):
+        """Return the dimension of the vector."""
+        return len(self._coords)
+
+    def __getitem__(self, j):
+        """Return jth coordinate of vector."""
+        return self._coords[j]
+
+    def __setitem__(self, j, val):
+        """Set jth coordinate of vector to given value."""
+        self._coords[j] = val
+
+    def __add__(self, other):
+        """Return sum of two vectors"""
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __sub__(self, other):
+        """Return difrence between two vectors"""
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] - other[j]
+        return result
+
+    def __eq__(self,other):
+        """Return True if vector has same coordinates as other."""
+        return self._coords == other._coords
+
+    def __ne__(self, other):
+        """Return True if vector diﬀers from other."""
+        return not self == other
+
+    def __str__(self):
+        """Produce string representation of vector."""
+        return '<' + str(self._coords)[1:-1] + '>'
+
+
+if __name__ == '__main__':
+    test = Vector(5)
+    print(test)
