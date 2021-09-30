ce39a7558b27862c45aaa7486a96e8285831658c
raed <eng.salhab@gmail.com>
2021-09-30 08:09:27 +0300

    Add exercise text as comment

--- Data_structures_and_Algorithms_in_python/ch3/R_3_1.py
+++ Data_structures_and_Algorithms_in_python/ch3/R_3_1.py
@@ -1,3 +1,7 @@
+"""R-3.1 Graph the functions 8n, 4n log n, 2n 2 , n 3 , and 2 n using a logarithmic scale
+for the x- and y-axes; that is, if the function value f (n) is y, plot this as a
+point with x-coordinate at log n and y-coordinate at log y."""
+
 from cProfile import label
 from tkinter import N
 import matplotlib.pyplot as plt
@@ -6,7 +10,7 @@ import numpy as np
 n = np.linspace(0, 10, 50)
 fig, ax = plt.subplots()
 plt.plot(n, 8 * n, label='linear') 
-plt.plot(n, 4 * n * np.log(n), label='n log n') 
+plt.plot(n, 4 * n * np.log2(n), label='n log n') 
 plt.plot(n, 2*n**2, label='quadratic')
 plt.plot(n, n**3, label='cubic')
 plt.plot(n, 2**n, label='exponential')
